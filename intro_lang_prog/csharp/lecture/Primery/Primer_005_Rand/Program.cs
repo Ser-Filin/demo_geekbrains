06741302c3140de464aac8053214914e212430d2
Ser Filin <ser_fil@rambler.ru>
2023-07-19 22:46:47 +0400

    Создан проект со случайностями

---
+++ intro_lang_prog/csharp/lecture/Primery/Primer_005_Rand/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10); // Случайное число 1 2 3 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
