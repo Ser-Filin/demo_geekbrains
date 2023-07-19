1306665f9541e388916e065e2b6f65b4a757cad5
Ser Filin <ser_fil@rambler.ru>
2023-07-20 02:59:53 +0400

    Добавлен проект Primer_007_Giri

---
+++ intro_lang_prog/csharp/lecture/Primery/Primer_007_Giri/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
