4b8b754276408c0cccf4055ba6bbb8259350871a
Ser Filin <ser_fil@rambler.ru>
2023-07-20 03:38:37 +0400

    Добавлен проект Primer_008_Tochki

---
+++ intro_lang_prog/csharp/lecture/Primery/Primer_008_Tochki/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("*");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("*");
+
+int x = xa, y = ya;
+int count = 0;
+
+while (count < 10000)
+{
+    int versh = new Random().Next(0, 3);
+
+    if (versh == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+        if (versh == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+        if (versh == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("*");
+    count++;
+}
