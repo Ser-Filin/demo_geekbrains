6a1d87a195ea94c785378b371873a918d7295247
Ser Filin <ser_fil@rambler.ru>
2023-07-19 23:10:34 +0400

    Создан проект с ветвлением if else + маппинг регистра символа

---
+++ intro_lang_prog/csharp/lecture/Primery/Primer_006_Uslovie/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Пожалуйста, представьтесь ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
