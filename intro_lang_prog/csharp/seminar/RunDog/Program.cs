0bace224c85dffe89c54ae84b06502b56be465a1
Ser Filin <ser_fil@rambler.ru>
2023-07-21 04:10:56 +0400

    Добавлен проект про бегущую собаку

---
+++ intro_lang_prog/csharp/seminar/RunDog/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача, блок схема которой была рассмотрена на одной из лекций.
+Написать программу, подсчитывающую сколько раз пробежит собака
+между друзьями, идущими навстречу друг другу.
+*/
+
+int distance = 10000, firstFriendSpeed = 1, secondFriendSpeed = 2,
+    dogSpeed = 5, friend = 2, count = 0, time;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance -= (firstFriendSpeed + secondFriendSpeed) * time;
+    count ++;
+}
+
+Console.WriteLine("Собака пробежит " + count + " раз.");
