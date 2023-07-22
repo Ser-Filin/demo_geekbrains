80e7eb4a88c1b97d77069999239e23e066089be2
Ser Filin <ser_fil@rambler.ru>
2023-07-22 22:50:55 +0400

    Добавлены лекционные примеры Primer_009 - 011

---
+++ intro_lang_prog/csharp/lecture/Primery/Primer_009_IntroMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (result < arg2) result = arg2;
+    if (result < arg3) result = arg3;
+    return result;
+}
+
+int a1 = 112125;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 2311;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23111;
+int c3 = 313;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
