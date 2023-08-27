05b4f763f39eefa14ca3eac169bf8dedf69f84f6
Ser Filin <ser_fil@rambler.ru>
2023-08-27 04:31:02 +0400

    Решена задача № 47 к 7-му семинару

---
+++ intro_lang_prog/csharp/seminar/homework007/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+// Количество строк и столбцов не могут принимать отрицательные
+// значения, поэтому ограничиваем ввод беззнаковыми данными.
+
+uint Invitation(string outText)
+{
+    Console.Write(outText);
+    uint inNumber = Convert.ToUInt32(Console.ReadLine());
+
+    return inNumber;
+}
+
+void GeneratedArray(uint rows, uint columns)
+{
+    float[,] array = new float[rows, columns];
+    sbyte[] multipliers = { -1, 1 }; // массив множителей для смены знака
+    Random rnd = new Random();
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < columns; c++)
+        {
+            array[r, c] = Convert.ToSingle(rnd.NextDouble() * 100);
+            int indexMult = rnd.Next(0, 2); // выбираем случайный множитель
+            array[r, c] *= multipliers[indexMult]; // меняем, или оставляем знак
+        }
+    }
+
+    // Вывод массива на экран:
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < columns; c++)
+            Console.Write(Math.Round(array[r, c], 2) + "; "); // ограничиваем длину числа 2-мя знаками
+                                                              // после запятой
+
+        Console.WriteLine();
+    }
+}
+
+uint numbersOfRows = Invitation("Введите количество строк: ");
+uint numbersOfColumns = Invitation("Введите количество столбцов: ");
+Console.WriteLine();
+
+Console.WriteLine("Массив создан:\n");
+GeneratedArray(numbersOfRows, numbersOfColumns);
+Console.WriteLine();
---
+++ intro_lang_prog/csharp/seminar/seminar007/Program.cs
@@ -0,0 +1,109 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+// int [,] CreateRandom2dArray(int rows, int columns, int MinValue, MaxValue)
+// {
+//     int[,] array = new int[rows, columns];
+
+//     for(int i = 0; i < rows; i++)
+
+//         for(int j = 0; j < columns; j++)
+//             array[i,j] = new Random().Next (MinValue, MaxValue + 1);
+
+//         return array;
+// }
+
+// void SHow2dArray(int [,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; < array.GetLength(1); j++)
+//         Console.WriteLine(array[i,j] + " ");
+
+//         Console.WriteLine();
+//     }
+// }
+// Console.WriteLine("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("");
+// int min = Convert.ToInt32Console.(ReadLine());
+// Console.WriteLine("");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+
+
+/*
+// 1. Задайте двумерный массив размера m на n, каждый
+// элемент в массиве находится по формуле: Aij = i+j.
+// Выведите полученный массив на экран.
+
+int Invitation(string outText)
+{
+    Console.Write(outText);
+    int inNumber = Convert.ToInt32(Console.ReadLine());
+
+    return inNumber;
+}
+
+void ComputeArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int j = 0; j < rows; j++)
+    {
+        for (int i = 0; i < columns; i++)
+            array[j, i] = j + i;
+    }
+
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+            Console.Write(array[j,i] + " ");
+
+        Console.WriteLine();
+    }
+}
+
+int inM = Invitation("Введите количество строк: ");
+int inN = Invitation("Введите количество столбцов: ");
+ComputeArray(inM, inN);
+*/
+
+
+// 2. Задайте двумерный массив. Найдите элементы, у которых
+// оба индекса чётные, и замените эти элементы на их квадраты.
+
+int Invitation(string outText)
+{
+    Console.Write(outText);
+    int inNumber = Convert.ToInt32(Console.ReadLine());
+
+    return inNumber;
+}
+
+int[,] GenArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return array;
+}
+
+int[,] SecondArray(int[,] array)
+{
+
+}
+
+int inM = Invitation("Введите количество строк: ");
+int inN = Invitation("Введите количество столбцов: ");
+int min = Invitation("Введите минимальное значение: ");
+int max = Invitation("Введите максимальное значение: ");
+
+// 3. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
+// (вне зависимости от размеров матрицы) и т.д.
