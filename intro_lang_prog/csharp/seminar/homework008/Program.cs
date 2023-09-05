f04327a490a712abd7ba9979ec0a70204631568d
Ser Filin <ser_fil@rambler.ru>
2023-09-06 03:10:54 +0400

    Решена задача № 56 к 8-му семинару.

--- intro_lang_prog/csharp/seminar/homework008/Program.cs
+++ intro_lang_prog/csharp/seminar/homework008/Program.cs
@@ -1,5 +1,105 @@
 ﻿using System;
 
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет
+находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+
+/*
+int[,] СreatingArray()
+{
+    Random rnd = new Random();
+
+    int row = rnd.Next(5, 16); // генерируются количество строк и столбцов
+    int column = rnd.Next(5, 16); // в промежутке от 5 до 15 строк/столбцов
+
+    while (row == column) // исключение создания квадратной матрицы
+    {
+        column = rnd.Next(5, 15);
+    }
+
+    int[,] quadArray = new int[row, column];
+
+    for (int i = 0; i < row; i++) // генерируется массив с числами от -20 до 20
+    {
+        for (int j = 0; j < column; j++)
+            quadArray[i, j] = rnd.Next(-20, 21);
+    }
+
+    return quadArray;
+}
+
+void ShowArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+            Console.Write(inArray[i, j] + "; ");
+        Console.WriteLine();
+    }
+}
+
+void RowNumber(int[,] inArray)
+{
+    int row = 0,
+        sum = 0;
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        int nextSum = 0;
+
+        for (int j = 0; j < inArray.GetLength(1); j++)
+            nextSum += inArray[i, j];
+
+        if (sum > nextSum)
+        {
+            sum = nextSum;
+            row = i;
+        }
+    }
+    Console.WriteLine($"номер строки с наименьшей суммой элементов: {row + 1}.");
+}
+
+ConsoleKeyInfo input;
+
+Console.WriteLine(
+    "Программа создаёт прямоугольный массив и выводит номер строки с наименьшей "
+        + "суммой элементов.\n"
+);
+
+Repeat:
+
+int[,] genArray = СreatingArray();
+
+Console.WriteLine("В созданном массиве:\n");
+ShowArray(genArray);
+Console.WriteLine();
+RowNumber(genArray);
+Console.WriteLine();
+
+Repeat2:
+
+Console.Write("Создать новый массив (y/n)? ");
+
+input = Console.ReadKey();
+Console.WriteLine();
+
+if (input.Key == ConsoleKey.Y)
+{
+    Console.Clear();
+    goto Repeat;
+}
+if (input.Key != ConsoleKey.N)
+    goto Repeat2;
+*/
+
+
 /*Задача 58: Задайте две матрицы. Напишите программу,
 которая будет находить произведение двух матриц.
 Например, даны 2 матрицы:
@@ -270,7 +370,7 @@ int[,,] Creating3DArray(int[] inMetric)
                 } while (checkArray[temp - 10] != 0);
 
                 array3D[r, c, d] = temp;
-                checkArray[temp - 10] = 1;
+                checkArray[temp - 10] = 1; // отметка о наличии числа
             }
         }
     }
